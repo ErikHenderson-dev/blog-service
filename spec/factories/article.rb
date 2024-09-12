6e165932b5bb9d4b9f923f9db5cd6cbe792d335d
ErikHenderson-dev <erikhenderson.dev@gmail.com>
2024-09-11 21:10:16 -0300

    feat: add article update service

---
+++ spec/factories/article.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :article do
+    id { Faker::Number.digit }
+    category_id { Faker::Number.digit }
+    title { Faker::Lorem.sentence }
+    sub_title { Faker::Lorem.sentence }
+    text { Faker::Lorem.paragraph }
+    author { Faker::Name.name }
+
+    association :category
+  end
+end
